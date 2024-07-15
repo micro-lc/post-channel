f9d24cb99cdfe93da9374b4b69aade13dcb09df5
Umberto Toniolo <amountainram@gmail.com>
2024-07-15 15:31:48 +0200

    chore(ci): add `dep-version` script

---
+++ scripts/dep-version.ts
@@ -0,0 +1,39 @@
+import { readFile } from 'fs/promises'
+import { createRequire } from 'module'
+
+import { Command } from 'commander'
+
+const require = createRequire(import.meta.url)
+
+function getArgs() {
+  const program = new Command()
+  const ctx: {dep?: string} = {}
+
+  program
+    .name('dep-version')
+    .description('Returns `package.json` dependency currently installed version')
+    .argument('<dep>', `dependency name`)
+    .action((dep) => {
+      typeof dep === 'string' && (ctx.dep = dep)
+    })
+    .parse()
+
+  return ctx
+}
+
+async function main(): Promise<string> {
+  const { dep: packageName } = getArgs()
+
+  if (packageName === undefined) {
+    throw new TypeError('must select a dependency')
+  }
+
+  const pathToPackageJSON = require.resolve(`${packageName}/package.json`)
+  const buffer = await readFile(pathToPackageJSON)
+  const { version } = JSON.parse(buffer.toString()) as {version: string}
+  return version
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
